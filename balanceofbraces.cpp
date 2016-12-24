637bf548797cb5bd7001fef9cde334c89fab3a8c
Catharsis96 <Alex@192.168.1.45>
2016-12-24 21:43:13 +0300

    hey

--- balanceofbraces.cpp
+++ balanceofbraces.cpp
@@ -1,31 +1,31 @@
 #include "stdafx.h"
-#include &lt;iostream&gt;
-#include &lt;fstream&gt;
-#include &lt;clocale&gt;
+#include <iostream>
+#include <fstream>
+#include <clocale>
 using namespace std;
 
 int main(int argc, _TCHAR* argv[])
 {
 	setlocale(LC_CTYPE,"rus");
 	char nazv[100];// строковой массив в которое собственно говоря будет записываться название файла
-	cout&lt;&lt;"Введите название файла:";
-	cin&gt;&gt;nazv;
+	cout << "Введите название файла:";
+	cin >> nazv;
 	ifstream inpfile(nazv);// открываем файл для чтения
 	if(!inpfile.is_open())
-		cout&lt;&lt;"Файл не может быть открыт";
+		cout << "Файл не может быть открыт";
 	else
 	{
 		int kolotkrck(0);//счетчик количества {
 		int kolzakrck(0);//счетчик количества } изначально равный нулю
 		char symv;// переменная для считывания символов из входного файла
-		inpfile&gt;&gt;symv; // считываем один символ из входного файла
+		inpfile>>symv; // считываем один символ из входного файла
 		while(inpfile)
 		{
 			if(symv=='{')kolotkrck++;
 			if(symv=='}')
 				{
 					kolzakrck++;
-					if(kolzakrck&gt;kolotkrck)break;
+					if(kolzakrck!=kolotkrck)break;
 			}
 			inpfile&gt;&gt;symv;//считываем следующий символ
 
